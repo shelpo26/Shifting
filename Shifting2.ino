d5317053dfa7817df8ecbfa3f937fdd001722f9d
Adrian Lobo <alf8aml04@gmail.com>
2023-02-21 18:49:18 -0500

    Code works, don't fucking touch it

--- Shifting2.ino
+++ Shifting2.ino
@@ -22,32 +22,30 @@ void loop() {
   byte GREEN_STATE_BUTTON = digitalRead(GREEN_BUTTON_PIN);
   byte RED_STATE_BUTTON = digitalRead(RED_BUTTON_PIN);
 
-      if (GREEN_STATE_BUTTON && RED_STATE_BUTTON == HIGH) {
-        if ( (millis() - lastDebounceTime) > debounceDelay) {
-        digitalWrite(GREEN_LED_PIN, LOW);
-        digitalWrite(RED_LED_PIN, LOW);
-        lastDebounceTime = millis();
-        }
-      }
-    
-    if (GREEN_STATE_BUTTON != lastGreenState) {
-      lastGreenState = GREEN_STATE_BUTTON;
-      if ( (millis() - lastDebounceTime) > debounceDelay) {
-        if (GREEN_STATE_BUTTON == HIGH){
-          digitalWrite(GREEN_LED_PIN, HIGH);
-          delay(250);
-          digitalWrite(GREEN_LED_PIN, LOW);
-        }
-        else {
-          digitalWrite(GREEN_LED_PIN, LOW);
-        }
-        lastDebounceTime = millis();
-      }else{
-        digitalWrite(GREEN_LED_PIN, LOW);
-      }
+  if (GREEN_STATE_BUTTON && RED_STATE_BUTTON == HIGH) {
+    if ( (millis() - lastDebounceTime) > debounceDelay) {
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(RED_LED_PIN, LOW);
+    lastDebounceTime = millis();
     }
+  }
 
-
+  if (GREEN_STATE_BUTTON != lastGreenState) {
+    lastGreenState = GREEN_STATE_BUTTON;
+    if ( (millis() - lastDebounceTime) > debounceDelay) {
+      if (GREEN_STATE_BUTTON == HIGH){
+        digitalWrite(GREEN_LED_PIN, HIGH);
+        delay(250);
+        digitalWrite(GREEN_LED_PIN, LOW);
+      }
+      else {
+        digitalWrite(GREEN_LED_PIN, LOW);
+      }
+      lastDebounceTime = millis();
+    }else{
+      digitalWrite(GREEN_LED_PIN, LOW);
+    }
+  }
 
   if (RED_STATE_BUTTON != lastRedState) {
     lastRedState = RED_STATE_BUTTON;
